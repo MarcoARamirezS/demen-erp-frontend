99e9713039536861fde2ddb5982186ccee2276f8
Marco Ramirez <marco.ramirez@ugto.mx>
2026-01-23 11:55:34 -0600

    [FIXED]: fix auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,16 @@
 export default defineNuxtRouteMiddleware(async to => {
   const auth = useAuthStore()
 
-  // Permitir login siempre
   if (to.path === '/login') return
 
-  // Si no hay token → login
   if (!auth.accessToken) {
     return navigateTo('/login')
   }
 
-  // Si hay token pero no usuario cargado
-  if (!auth.userId) {
+  if (!auth.user && !auth.loading) {
     try {
       await auth.fetchMe()
     } catch {
-      auth.logout()
       return navigateTo('/login')
     }
   }
